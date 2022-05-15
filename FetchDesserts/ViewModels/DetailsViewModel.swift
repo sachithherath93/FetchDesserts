950a634bd0e8ef43938c4cae72f09b3a306ca399
sachithherath <sachithrajakaruna@gmail.com>
2022-05-14 22:00:32 -0500

    Deal with empty strings

--- FetchDesserts/ViewModels/DetailsViewModel.swift
+++ FetchDesserts/ViewModels/DetailsViewModel.swift
@@ -31,8 +31,9 @@ class DetailsViewModel {
     }
     
     func getIngredientsAndMeasurements(item: MealItem) -> String {
-        let ingredients = [item.strIngredient1, item.strIngredient2, item.strIngredient3, item.strIngredient4, item.strIngredient5, item.strIngredient6, item.strIngredient7, item.strIngredient8, item.strIngredient9, item.strIngredient10, item.strIngredient11, item.strIngredient12, item.strIngredient13, item.strIngredient14, item.strIngredient15, item.strIngredient16, item.strIngredient17, item.strIngredient18, item.strIngredient19, item.strIngredient20].compactMap { $0 }.filter { !$0.isEmpty }
-        let measurements = [item.strMeasure1, item.strMeasure2, item.strMeasure3, item.strMeasure4, item.strMeasure5, item.strMeasure6, item.strMeasure7, item.strMeasure8, item.strMeasure9, item.strMeasure10, item.strMeasure11, item.strMeasure12, item.strMeasure13, item.strMeasure14, item.strMeasure15, item.strMeasure16, item.strMeasure17, item.strMeasure18, item.strMeasure19, item.strMeasure20].compactMap { $0 }.filter { !$0.isEmpty }
+        let ingredients = [item.strIngredient1, item.strIngredient2, item.strIngredient3, item.strIngredient4, item.strIngredient5, item.strIngredient6, item.strIngredient7, item.strIngredient8, item.strIngredient9, item.strIngredient10, item.strIngredient11, item.strIngredient12, item.strIngredient13, item.strIngredient14, item.strIngredient15, item.strIngredient16, item.strIngredient17, item.strIngredient18, item.strIngredient19, item.strIngredient20].compactMap { $0 }.filter {
+            !$0.isEmpty && $0 != " " }
+        let measurements = [item.strMeasure1, item.strMeasure2, item.strMeasure3, item.strMeasure4, item.strMeasure5, item.strMeasure6, item.strMeasure7, item.strMeasure8, item.strMeasure9, item.strMeasure10, item.strMeasure11, item.strMeasure12, item.strMeasure13, item.strMeasure14, item.strMeasure15, item.strMeasure16, item.strMeasure17, item.strMeasure18, item.strMeasure19, item.strMeasure20].compactMap { $0 }.filter { !$0.isEmpty && $0 != " " }
         return combineIngredientsAndMeasurements(ingredients: ingredients, measurements: measurements)
     }
     
